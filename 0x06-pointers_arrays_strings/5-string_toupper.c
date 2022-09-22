968a1ac371d82e8fe05b7137831e5ac427cfd72f
Jennifer Ajibo <jenniferajibo@gmail.com>
2022-09-22 16:16:12 -0700

    Changes all lowercase letters to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *string_toupper- capitalizes all words of a string
+ *@s: string
+ *
+ *Return: String
+ */
+
+char *string_toupper(char *s)
+{
+	int c = 0;
+
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		if (s[c] >= 65 && s[c] <= 90)
+		{
+			s[c] = s[c] + 32;
+		}
+		if (s[c] >= 97 && s[c] <= 122)
+		{
+			s[c] = s[c] - 32;
+		}
+	}
+	return (s);
+}
